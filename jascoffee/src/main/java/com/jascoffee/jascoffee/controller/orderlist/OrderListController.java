9dbee7528dbc4491625795ba851f8aa36914e2ef
naemhui <knaem.edu@gmail.com>
2024-12-15 01:30:56 +0900

    Feat: OrderListController 작성

--- jascoffee/src/main/java/com/jascoffee/jascoffee/controller/orderlist/OrderListController.java
+++ jascoffee/src/main/java/com/jascoffee/jascoffee/controller/orderlist/OrderListController.java
@@ -1,4 +1,37 @@
 package com.jascoffee.jascoffee.controller.orderlist;
 
+import com.jascoffee.jascoffee.dto.orderlist.request.OrderListCreateRequest;
+import com.jascoffee.jascoffee.dto.orderlist.request.OrderListUpdateRequest;
+import com.jascoffee.jascoffee.dto.orderlist.response.OrderListResponse;
+import com.jascoffee.jascoffee.service.orderlist.OrderListService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigInteger;
+
+@RestController
+@RequestMapping("/api/orderlist")
+@RequiredArgsConstructor
 public class OrderListController {
+
+    private final OrderListService orderListService;
+
+    // 주문 생성
+    @PostMapping
+    public ResponseEntity<OrderListResponse> createOrder(@RequestBody OrderListCreateRequest request) {
+        // service 호출 -> 주문 생성
+        OrderListResponse response = orderListService.createOrder(request);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    // 주문 수정
+    @PutMapping("/{orderID}")
+    public ResponseEntity<OrderListResponse> updateOrder(@PathVariable BigInteger orderID, @RequestBody OrderListUpdateRequest request) {
+        request.setOrderID(orderID);
+
+        OrderListResponse response = orderListService.updateOrder(request);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 }
