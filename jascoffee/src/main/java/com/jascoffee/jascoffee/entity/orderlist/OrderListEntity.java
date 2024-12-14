5e60f4466e81ab65c3658613da96f87a34f4f1bf
naemhui <knaem.edu@gmail.com>
2024-12-15 01:02:14 +0900

    Fix: OrerListEntity 롬복 사용해서 수정

--- jascoffee/src/main/java/com/jascoffee/jascoffee/entity/orderlist/OrderListEntity.java
+++ jascoffee/src/main/java/com/jascoffee/jascoffee/entity/orderlist/OrderListEntity.java
@@ -1,18 +1,24 @@
 package com.jascoffee.jascoffee.entity.orderlist;
 
 import jakarta.persistence.*;
+import lombok.*;
+
 import java.math.BigInteger;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "orderlist")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class OrderListEntity {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private BigInteger orderID;
 
-    @Column(nullable = false)  // NOT NULL 제약 조건
+    @Column(nullable = false) // NOT NULL 제약 조건
     private BigInteger userID;
 
     @Column
@@ -23,44 +29,4 @@ public class OrderListEntity {
 
     @Column
     private LocalDateTime orderedAt;
-
-    // 기본 생성자
-    public OrderListEntity() {
-    }
-
-    public BigInteger getOrderID() {
-        return orderID;
-    }
-
-    public void setOrderID(BigInteger orderID) {
-        this.orderID = orderID;
-    }
-
-    public BigInteger getUserID() {
-        return userID;
-    }
-
-    public void setUserID(BigInteger userID) {
-        this.userID = userID;
-    }
-
-    public Integer getTotalPrice() {
-        return totalPrice = totalPrice;
-    }
-
-    public Boolean getIsCancel() {
-        return isCancel;
-    }
-
-    public void setIsCancel(Boolean isCancel) {
-        this.isCancel = isCancel;
-    }
-
-    public LocalDateTime getOrderedAt() {
-        return orderedAt;
-    }
-
-    public void setOrderedAt(LocalDateTime orderedAt) {
-        this.orderedAt = orderedAt;
-    }
 }
