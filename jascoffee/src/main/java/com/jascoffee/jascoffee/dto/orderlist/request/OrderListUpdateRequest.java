e52069af307768bfab24bf76fb3708a4210761b0
naemhui <knaem.edu@gmail.com>
2024-12-15 00:41:32 +0900

    OrderList dto 작성

--- jascoffee/src/main/java/com/jascoffee/jascoffee/dto/orderlist/request/OrderListUpdateRequest.java
+++ jascoffee/src/main/java/com/jascoffee/jascoffee/dto/orderlist/request/OrderListUpdateRequest.java
@@ -1,4 +1,14 @@
 package com.jascoffee.jascoffee.dto.orderlist.request;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigInteger;
+
+@Data
+@NoArgsConstructor
 public class OrderListUpdateRequest {
+    private BigInteger orderID;
+    private Integer totalPrice;
+    private Boolean isCancel;
 }
