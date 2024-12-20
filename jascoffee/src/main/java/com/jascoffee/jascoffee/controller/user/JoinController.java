086d1cd42ea9b15a6f7796eb4661d88e732b47af
shinheewon0107 <shinheewon0107@gmail.com>
2024-12-20 20:00:56 +0900

    [희원] Fix: Http메소드 수정

--- jascoffee/src/main/java/com/jascoffee/jascoffee/controller/user/JoinController.java
+++ jascoffee/src/main/java/com/jascoffee/jascoffee/controller/user/JoinController.java
@@ -1,10 +1,14 @@
 package com.jascoffee.jascoffee.controller.user;
 
 import com.jascoffee.jascoffee.dto.user.JoinDTO;
+import com.jascoffee.jascoffee.entity.user.UserEntity;
+import com.jascoffee.jascoffee.jwt.JWTUtil;
+import com.jascoffee.jascoffee.repository.user.UserRepository;
 import com.jascoffee.jascoffee.service.user.JoinService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController  // @Controller + @ResponseBody 결합
 public class JoinController {
@@ -28,5 +32,18 @@ public class JoinController {
 //        joinService.joinProcess(joinDTO);
 //        return "ok";
 //    }
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+
+    // POST 요청으로 account를 받아서 회원을 조회하고 반환
+    @GetMapping("/user")
+    public String getUserByAccount(@RequestHeader String access) {
+        // account를 통해 DB에서 회원 정보 조회
+        String account = jwtUtil.getAccount(access);
+
+        return account;
+    }
 
 }
